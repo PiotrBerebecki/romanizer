14abd7a6b57c2f3788a8980131435646aaae27b0
PiotrBerebecki <petebere@gmail.com>
2017-02-27 16:04:27 +0000

    Add solution

--- index.js
+++ index.js
@@ -1,3 +1,88 @@
 'use strict';
 
-// write your code here
+function convertToRoman(n) {
+
+  var romanNumbers = {
+    'M': 1000,
+    'CM': 900,
+    'D':  500,
+    'CD': 400,
+    'C':  100,
+    'XC':  90,
+    'L':   50,
+    'XL':  40,
+    'X':   10,
+    'IX':   9,
+    'V':    5,
+    'IV':   4,
+    'I':    1
+  };
+
+  var result = '';
+
+  if (isNaN(n)) {
+    return 'Not a number';
+  }
+
+  if (n === 0) {
+    return 'No 0 in roman numerals';
+  }
+
+  if (n > 3888) {
+    return 'Cannot take number greater than 3888';
+  }
+
+  for (var number in romanNumbers){
+    while (n >= romanNumbers[number]){
+      result += number;
+      n = n - romanNumbers[number];
+    }
+  }
+
+  return result;
+}
+
+
+
+
+
+// Recursive approach
+function convertToRoman2(n) {
+
+  var romanNumbers = {
+    'M': 1000,
+    'CM': 900,
+    'D':  500,
+    'CD': 400,
+    'C':  100,
+    'XC':  90,
+    'L':   50,
+    'XL':  40,
+    'X':   10,
+    'IX':   9,
+    'V':    5,
+    'IV':   4,
+    'I':    1
+  };
+
+  var result = '';
+
+  if (isNaN(n)) {
+    return 'Not a number';
+  }
+
+  if (n === 0) {
+    return '';
+  }
+
+  if (n > 3888) {
+    return 'Cannot take number greater than 3888';
+  }
+
+  for (var number in romanNumbers){
+    if (n >= romanNumbers[number]){
+      result = number;
+      return result + convertToRoman(n - romanNumbers[number]);
+    }
+  }
+}
--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,62 @@
 // write your tests here
 
 
-  
+QUnit.test('There should be a function convertToRoman', function(assert) {
+  assert.equal(convertToRoman(1), 'I');
+});
+
+QUnit.test('2 should return II', function(assert) {
+  assert.equal(convertToRoman(2), 'II');
+});
+
+QUnit.test('3 should return III', function(assert) {
+  assert.equal(convertToRoman(3), 'III');
+});
+
+QUnit.test('4 should return IV', function(assert) {
+  assert.equal(convertToRoman(4), 'IV');
+});
+
+QUnit.test('5 should return V', function(assert) {
+  assert.equal(convertToRoman(5), 'V');
+});
+
+QUnit.test('6 should return VI', function(assert) {
+  assert.equal(convertToRoman(6), 'VI');
+});
+
+QUnit.test('9 should return IX', function(assert) {
+  assert.equal(convertToRoman(9), 'IX');
+});
+
+QUnit.test('10 should return X', function(assert) {
+  assert.equal(convertToRoman(10), 'X');
+});
+
+QUnit.test('20 should return X', function(assert) {
+  assert.equal(convertToRoman(20), 'XX');
+});
+
+QUnit.test('10 should return X', function(assert) {
+  assert.equal(convertToRoman(59), 'LIX');
+});
+
+QUnit.test('88 should return LXXXVIII', function(assert) {
+  assert.equal(convertToRoman(88), 'LXXXVIII');
+});
+
+QUnit.test('2367 should return MMCCCLXVII', function(assert) {
+  assert.equal(convertToRoman(2367), 'MMCCCLXVII');
+});
+
+QUnit.test('Hello should return Not a number', function(assert) {
+  assert.equal(convertToRoman('Hello'), 'Not a number');
+});
+
+QUnit.test('There is no 0 in roman numerals', function(assert) {
+  assert.equal(convertToRoman(0), 'No 0 in roman numerals');
+});
+
+QUnit.test('It does not return a roman number for numbers greater than 3888', function(assert) {
+  assert.equal(convertToRoman(3889), 'Cannot take number greater than 3888');
+});
